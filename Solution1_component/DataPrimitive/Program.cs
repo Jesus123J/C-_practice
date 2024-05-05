ece67eb59b5a48645446b06adec7496293237d2b
Thiago <gutierrezdelatorrej30@gmail.com>
2024-05-05 10:23:35 -0500

    structure add

---
+++ Solution1_component/DataPrimitive/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataPrimitive
+{
+    internal class Program
+    {
+        private static string name;
+        private static int age;
+        private static double salary;
+        private static bool merried;
+        static void Main(string[] args)
+        {
+            component();
+        }
+
+        private static void component(){
+            while (true)
+            {
+               Thread.Sleep(1000);
+               Console.WriteLine("Procesing component");
+            }
+        } 
+    }
+}
