99809c2ded78c577ecd600db97e627239e8a7d4f
Thiago <gutierrezdelatorrej30@gmail.com>
2024-10-07 01:05:14 -0500

    Add components to the “store_desktop_app” application folder

--- store_desktop_app/Form1.Designer.cs
+++ store_desktop_app/Form1.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(350, 95);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(350, 161);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(159, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(350, 196);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(159, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(318, 268);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 32);
+            button1.TabIndex = 3;
+            button1.Text = "Clear";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(251, 164);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 4;
+            label2.Text = "User name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(251, 199);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Password";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(438, 268);
+            button2.Name = "button2";
+            button2.Size = new Size(98, 32);
+            button2.TabIndex = 6;
+            button2.Text = "Login";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(233, 268);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 32);
+            button3.TabIndex = 7;
+            button3.Text = "Exit";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private Button button2;
+        private Button button3;
     }
 }
--- store_desktop_app/Form1.cs
+++ store_desktop_app/Form1.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
 namespace store_desktop_app
 {
     public partial class Form1 : Form
@@ -5,6 +10,59 @@ namespace store_desktop_app
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // SQL connection string
+        SqlConnection conn = new SqlConnection(@"Data Source=IDEVEXPERT;Initial Catalog=StoreDesktop;Integrated Security=True");
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get user input
+                string textUser = textBox1.Text;
+                string textPassword = textBox2.Text;
+
+                // Open connection
+                conn.Open();
+
+                // Use parameterized query to prevent SQL injection
+                string query = "SELECT * FROM Login_new WHERE username = @username AND password = @password";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@username", textUser);
+                cmd.Parameters.AddWithValue("@password", textPassword);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                // Check if the user exists
+                if (dataTable.Rows.Count > 0)
+                {
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    MenuForm menuForm = new MenuForm();
+                    menuForm.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Error de cuenta");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Show detailed error message
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                // Close connection
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
         }
     }
 }
